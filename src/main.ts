d5007a4848d5dceeeaec8515693de88309cecb58
Patryk Janik <patrykjanik1710@gmail.com>
2023-04-30 20:08:06 +0200

    Configured swagger doc as an example.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaService } from './prisma/prisma.service';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(7878);
+
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
+
+  const config = new DocumentBuilder()
+    .setTitle('Some custom doc example')
+    .setDescription('Some custom doc description')
+    .setVersion('1.0')
+    .addTag('Tag')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(7878);
 }
 bootstrap();
